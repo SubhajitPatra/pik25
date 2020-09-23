64468551aef82dc7bd3cd38185438062985eb167
Subhajit Patra <1souvikpa@gmail.com>
2020-09-23 09:31:36 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+    printf("My name is Subhajit Patra.\n");
+    printf("I read in class 9.\n");
+    printf("I like to read stories of Sherlock Holmes.\n");
+    printf("My favorite sport is chess.\n");
+    printf("tell me the number I will tell you it's square.\n");
+    scanf("%d",&number);
+    printf("My favorite number is %d",number*number);
+    return 0;
+}
+
